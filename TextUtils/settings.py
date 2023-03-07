91b3e242a5e59c29eacd7fd1dc801a87a10076ae
amoldhoke <amoldhoke07@gmail.com>
2023-03-07 22:42:01 +0530

    .

--- TextUtils/settings.py
+++ TextUtils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7riv%65x(t9nq)62neo_$_gh*q6*p9gl*z78lrxxaxg62w)lke
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
