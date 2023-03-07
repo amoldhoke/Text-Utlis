d58a0db4511200b692a46b56bfc5c1475009da48
amoldhoke <amoldhoke07@gmail.com>
2023-03-07 23:37:22 +0530

    .

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from home import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', views.analyze, name='analyze'),
     path('analyze/', views.output, name='output'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
